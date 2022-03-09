e43b58b35df4a65b96272baaec053520a9f3ea8c
Parthasai <53685926+propardhu@users.noreply.github.com>
2022-03-09 22:22:07 +0530

    tables work

--- src/App.vue
+++ src/App.vue
@@ -22,4 +22,37 @@ export default defineComponent({
   color: #2c3e50;
   margin-top: 60px;
 }
+table {
+  font-family: 'Open Sans', sans-serif;
+  width: 75%;
+  border-collapse: collapse;
+  border: 3px solid #44475C;
+  margin-right: auto !important;
+ margin-left: auto !important;
+}
+
+table th {
+  text-transform: uppercase;
+  text-align: left;
+  background: #44475C;
+  color: #FFF;
+  padding: 8px;
+  min-width: 30px;
+}
+
+table td {
+  text-align: left;
+  padding: 8px;
+  border-right: 2px solid #7D82A8;
+}
+table td:last-child {
+  border-right: none;
+}
+table tbody tr:nth-child(2n) td {
+  background: #D4D8F9;
+}
+input {
+    margin: .4rem;
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,77 @@
 <template>
-    <h1>Home</h1>
+  <h1>Home</h1>
+  <table id="secondTable">
+    <thead>
+      <tr>
+        <th v-for="col in columns" :key="col.id">
+            <input v-if="col=='id'" v-model="selectall" type="checkbox">
+            {{ col }}
+        </th>
+        <th>
+            actions
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="row in rows" :key="row['id']">
+        <td v-for="col in columns" :key="col">
+            <input v-if="col=='id'" v-model="row['selected']" type="checkbox">
+            {{ row[col] }}
+        </td>
+        <td>
+            <button @click="remove(row['id'])">delete</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent,computed,reactive, ref, watch } from 'vue'
 
 export default defineComponent({
     setup() {
+        const rows = reactive(
+            [
+                { id: 1, name: "Chandler Bing", Company: '305-917-1301', Status: 'Active',Last_Updated:"1/1/2022",Notes:"hello all",selected: false },
+                { id: 2, name: "Ross Geller", Company: '210-684-8953', Status: 'Active',Last_Updated:"1/1/2022",Notes:"hello all",selected: false },
+                { id: 3, name: "Rachel Green", Company: '765-338-0312', Status: 'Active',Last_Updated:"1/1/2022",Notes:"hello all",selected: false},
+                { id: 4, name: "Monica Geller", Company: '714-541-3336', Status: 'Active',Last_Updated:"1/1/2022",Notes:"hello all",selected: false },
+                { id: 5, name: "Joey Tribbiani", Company: '972-297-6037', Status: 'Active',Last_Updated:"1/1/2022",Notes:"hello all",selected: false },
+                { id: 6, name: "Phoebe Buffay", Company: '760-318-8376', Status: 'Active',Last_Updated:"1/1/2022",Notes:"hello all",selected: false }
+            ]);
         
+        const columns = computed(() => {
+            if (rows.length == 0) {
+                return [];
+            }
+            return Object.keys(rows[0]);
+        });
+        
+        const selectall = ref(false);
+        watch(selectall,(newValue)=>{
+            for(var i in rows){
+                rows[i]['selected'] = newValue;
+            }
+        });
+
+        const remove = (i) => {
+            var temp = 0;
+            for(var j in rows){
+                if(rows[j]['id']==i){
+                    temp = j;
+                    break;
+                }
+            }
+            rows.splice(temp,1);
+            
+        }
+
+        return {
+            selectall,
+            rows,
+            columns,
+            remove
+        }
     },
 })
 </script>
